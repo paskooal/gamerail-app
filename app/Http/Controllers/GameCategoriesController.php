78ee6fe0ff263f094ff1fac671372ce3b3c9d8eb
EduardoGelo <eduardo24games@gmail.com>
2025-04-02 23:19:51 -0300

    tudo preparado, so falta terminar de configurar

--- app/Http/Controllers/GameCategoriesController.php
+++ app/Http/Controllers/GameCategoriesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CompanieController extends Controller
+class GameCategoriesController extends Controller
 {
     /**
      * Display a listing of the resource.
--- app/Http/Controllers/ProdCategoriesController.php
+++ app/Http/Controllers/ProdCategoriesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CompanieController extends Controller
+class ProdCategoriesController extends Controller
 {
     /**
      * Display a listing of the resource.
