89a7e8f89510e628ea9fbc92097eaebae632d8ab
EduardoGelo <eduardo24games@gmail.com>
2025-04-12 15:05:07 -0300

    bug no edit do gamecategories

--- resources/views/game_category/edit.blade.php
+++ resources/views/game_category/edit.blade.php
@@ -1,1 +1,16 @@
-<h1>aqui serão editadas as categorias de jogos</h1>
+@extends('store')
+
+@section('content')
+
+<h1>aqui serão editados as categorias de jogos</h1>
+
+<form action="{{ route('gameCategories.update', ['gameCategory' => $gameCategory->id]) }}" method="post">
+    @csrf
+    @method('PUT')
+    <input type="text" name="name" value="{{ $gameCategory->name }}">
+    <input type="text" name="description" value="{{ $gameCategory->description }}">
+    <input type="text" name="image" value="{{ $gameCategory->image }}">
+    <button type="submit">Enviar</button>
+</form>
+
+@endsection
